8fbac191562438061c494c75c32652c74612d93d
ryden <ryferr8122@gmail.com>
2019-05-01 14:06:39 -0500

    set up front end requirements

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Client extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <div>Hullo!</div>
+      </>
+    )
+  }
+}
+
+export default Client;
