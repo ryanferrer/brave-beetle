2a116d274893a4ca604b1761c813861d56dad876
ryden <ryferr8122@gmail.com>
2019-05-07 13:04:13 -0500

    testing to see if multiple ReactDOMs work. Success

--- client/Latch.jsx
+++ client/Latch.jsx
@@ -2,5 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
+import Extra from './components/Extra.jsx';
 
 ReactDOM.render(<App />, document.getElementById('App'));
+// Can I do multiple ReactDOM renders for multiple utilities?
+// Then later I can recreate the website using micro services architecture.
+
+ReactDOM.render(<Extra/>, document.getElementById('Extra'));
---
+++ client/components/Extra.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Extra extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <> 
+        Well, look at me! I'm rendered! Whoa!
+      </>
+    );
+  }
+}
+
+export default Extra;
