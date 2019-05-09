82bb0f1c29b9ca704a38ec47dd62bb0431e4915a
ryden <ryferr8122@gmail.com>
2019-05-08 22:56:53 -0500

    Contained UBRoller in it's own div

--- client/Latch.jsx
+++ client/Latch.jsx
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx'
+import UBRoller from './components/UBRoller/UBRoller.jsx'
 import Extra from './components/Extra.jsx';
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(<UBRoller />, document.getElementById('UBRoller-Container'));
 // Can I do multiple ReactDOM renders for multiple utilities?
 // Then later I can recreate the website using micro services architecture.
 
